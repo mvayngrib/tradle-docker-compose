3f372a4f2a8763fd4a2c6602c331b80db8cdb628
Mark Vayngrib <mark@tradle.io>
2016-08-20 15:05:35 -0400

    fix forum

--- combined/forum.sh
+++ combined/forum.sh
@@ -6,4 +6,4 @@ source .env
 docker run --name forum-redis -p 6379 --net combined_default -d redis redis-server --appendonly yes
 
 # run forum on the same network as the nginx server (combined)
-docker run -it --name forum --link forum-redis:redis --net combined_default -e VIRTUAL_HOST=${TRADLE_FORUM_URL} -e VIRTUAL_PORT=4567 nodebb/docker:centos
+docker run -it --name forum --link forum-redis:redis --net combined_default -e VIRTUAL_HOST=${TRADLE_FORUM_URL} -e VIRTUAL_PORT=4567 tradle/forum
